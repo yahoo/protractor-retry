ea9f351eee825ee4bd78ef038103dc6490a7da31
dreuxl <dreuxl@oath.com>
2017-10-10 11:15:59 -0700

    fixing

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -13,7 +13,7 @@ exports.config = {
         browserName: 'firefox',
       //  tunnel-identifier: process.env.TRAVIS_JOB_NUMBER,
         build: process.env.TRAVIS_BUILD_NUMBER,
-        name: process.env.TRAVIS_BRANCH +'-'process.env.TRAVIS_BUILD_NUMBER
+        name: process.env.TRAVIS_BRANCH +'-'+process.env.TRAVIS_BUILD_NUMBER
     },
     sauceUser: process.env.SAUCE_USERNAME,
     sauceKey: process.env.SAUCE_ACCESS_KEY,
