c31e92426a01bb666e49e27ce050da60c93b85bc
Amro Tahtamouni <amro_t17@yahoo.com>
2017-10-13 15:23:12 -0700

    adding some jasmine testcases

---
+++ test/protractor.jasmine.conf.js
@@ -0,0 +1,27 @@
+/*global browser */
+var retry = require('../lib/retry');
+
+exports.config = {
+    framework: 'jasmine2',
+    specs: ['./jasmine/*.spec.js'],
+    capabilities: {
+        shardTestFiles: true,
+        maxInstances: 4,
+        browserName: 'firefox',
+        Build: 'protractor-retry-'+process.env.TRAVIS_BRANCH +'-'+process.env.TRAVIS_BUILD_NUMBER,
+        name: process.env.TRAVIS_BRANCH +'-'+process.env.TRAVIS_BUILD_NUMBER
+    },
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
+    onCleanUp: function (results) {
+        retry.onCleanUp(results);
+    },
+    onPrepare: function () {
+        retry.onPrepare();
+        require('jasmine-expect');
+	browser.ignoreSynchronization = true;
+    },
+    afterLaunch: function() {
+        return retry.afterLaunch(2); // number of retries ( default is 2 )
+    }
+};
---
+++ test/protractor.mocha.conf.js
@@ -0,0 +1,33 @@
+/*global browser */
+var retry = require('../lib/retry');
+
+exports.config = {
+    framework:'mocha',
+    mochaOpts: {
+        enableTimeouts: false
+    },
+    specs: ['./mocha/*.spec.js'],
+    capabilities: {
+        shardTestFiles: true,
+        maxInstances: 4,
+        browserName: 'firefox',
+        Build: 'protractor-retry-'+process.env.TRAVIS_BRANCH +'-'+process.env.TRAVIS_BUILD_NUMBER,
+        name: process.env.TRAVIS_BRANCH +'-'+process.env.TRAVIS_BUILD_NUMBER
+    },
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
+    onCleanUp: function (results) {
+        retry.onCleanUp(results);
+    },
+    onPrepare: function () {
+        retry.onPrepare();
+        var chai = require('chai');
+        var chaiAsPromised = require('chai-as-promised');
+        chai.use(chaiAsPromised);
+        global.expect = chai.expect;
+        browser.ignoreSynchronization = true;
+    },
+    afterLaunch: function() {
+        return retry.afterLaunch(2); // number of retries ( default is 2 )
+    }
+};
